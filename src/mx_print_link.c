312368f0e6c75a23f6bba249ce9149edf10d8639
ykovtun <ykovtun@stud.khpi.ucode-connect.study>
2023-11-15 12:14:46 +0200

    add extend file permissions

--- src/mx_print_link.c
+++ src/mx_print_link.c
@@ -3,7 +3,7 @@
 void mx_print_link(const char *name){
     char buff[PATH_MAX];
     ssize_t len = readlink(name,buff,sizeof(buff)-1);
-    if (len != -1){
+    if(len != -1){
         mx_printstr(" -> ");
         mx_printstr(buff);
     }
