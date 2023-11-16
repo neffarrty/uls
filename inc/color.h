0b7f029b831f5fdef819cc99089973aef0e050f0
ykovtun <ykovtun@stud.khpi.ucode-connect.study>
2023-11-16 20:15:55 +0200

    change color defines

--- inc/color.h
+++ inc/color.h
@@ -3,14 +3,27 @@
 
 #include "uls.h"
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
-#define BLU   "\x1B[34m"
-#define MAG   "\x1B[35m"
-#define CYN   "\x1B[36m"
-#define WHT   "\x1B[37m"
-#define RESET "\x1B[0m"
+/* #define RED   "\x1B[31m"
+// #define GRN   "\x1B[32m"
+// #define YEL   "\x1B[33m"
+// #define BLU   "\x1B[34m"
+// #define MAG   "\x1B[35m"
+// #define CYN   "\x1B[36m"
+// #define WHT   "\x1B[37m" */
+
+#define NON_COLOR       "\x1B[0m"
+#define DIR_COLOR       "\x1B[34m"
+#define LNK_COLOR       "\x1B[35m"
+#define SOC_COLOR       "\x1B[32m"
+#define PIP_COLOR       "\x1B[33m"
+#define EXE_COLOR       "\x1B[31m"
+#define BLK_COLOR       "\x1B[34;46m"
+#define CHR_COLOR       "\x1B[34;43m"
+#define EXE_UID_COLOR   "\x1B[30;41m"
+#define EXE_GID_COLOR   "\x1B[30;46m"
+#define DIR_STK_COLOR   "\x1B[30;42m"
+#define DIR_NTK_COLOR   "\x1B[30;43m"
+
 
 #endif
 
--- src/mx_print_color_name.c
+++ src/mx_print_color_name.c
@@ -3,30 +3,30 @@
 void mx_print_color_name(t_fileinfo file) {
     switch(file.st.st_mode & S_IFMT) {
         case S_IFSOCK:
-            mx_printstr(GRN);
+            mx_printstr(SOC_COLOR);
             break;
         case S_IFLNK:
-            mx_printstr(MAG);
+            mx_printstr(LNK_COLOR);
             break;
         case S_IFBLK:
-            mx_printstr(BLU);
+            mx_printstr(BLK_COLOR);
             break;
         case S_IFDIR:
-            mx_printstr(BLU);
+            mx_printstr(DIR_COLOR);
             break;
         case S_IFCHR:
-            mx_printstr(GRN);
+            mx_printstr(CHR_COLOR);
             break;
         case S_IFIFO:
-            mx_printstr(YEL);
+            mx_printstr(PIP_COLOR);
             break;
         default:
             if(file.st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) {       
-                mx_printstr(RED);
+                mx_printstr(EXE_COLOR);
             }
             break;
     }
     mx_printstr(file.name);
-    mx_printstr(RESET);
+    mx_printstr(NON_COLOR);
 }
 
