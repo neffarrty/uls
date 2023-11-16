6f04fac4e2843f502d7f635c0f3d683ed866b868
Andrii Zhupanov <Andrii@MacBook-Pro-Andrey.local>
2023-11-16 16:37:42 +0100

    fix mx_print_link

--- src/mx_print_link.c
+++ src/mx_print_link.c
@@ -3,11 +3,11 @@
 void mx_print_link(const char *path){
     char buff[PATH_MAX];
     ssize_t len = readlink(path, buff, sizeof(buff) - 1);
-    
+
     if(len != -1){
+        buff[len] = '\0';
         mx_printstr(" -> ");
         mx_printstr(buff);
     }
 }
-
 
