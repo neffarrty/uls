ee50555abc484352701cf80f8db218867f493f55
Andrii Zhupanov <Andrii@macbook-pro-andrey.home>
2023-11-16 21:47:19 +0100

    fix extended atributes output

--- src/mx_print_xattr.c
+++ src/mx_print_xattr.c
@@ -1,10 +1,10 @@
 #include "../inc/uls.h"
 
 void mx_print_xattr(const char *path) {
-    int len_of_atribute = listxattr(path, NULL, 0, XATTR_NOFOLLOW);
+    ssize_t len_of_atribute = listxattr(path, NULL, 0, XATTR_NOFOLLOW);
     char* xattr_list = malloc(len_of_atribute);
-    ssize_t xattr = listxattr(path, xattr_list, XATTR_MAXNAMELEN, XATTR_NOFOLLOW);
-    
+    ssize_t xattr = listxattr(path, xattr_list, len_of_atribute, XATTR_NOFOLLOW);
+
     if(xattr > 0) {
         for(char *list = xattr_list; list < xattr_list + xattr; list += strlen(list) + 1){
             int len = getxattr(path, list, NULL, 0, 0, XATTR_NOFOLLOW);
