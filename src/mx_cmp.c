4afc8afc975293b26711b803a1bb697a3edbd954
Andrii Zhupanov <Andrii@macbook-pro-andrey.home>
2023-11-22 13:30:43 +0100

    one more time fix sorting

--- src/mx_cmp.c
+++ src/mx_cmp.c
@@ -2,21 +2,21 @@
 
 bool mx_cmp_atime(t_fileinfo a, t_fileinfo b) {
     if(a.st.st_atime == b.st.st_atime) {
-        return mx_cmp_name(a, b);
+        return !mx_cmp_name(a, b);
     }
     return a.st.st_atime < b.st.st_atime;
 }
 
 bool mx_cmp_ctime(t_fileinfo a, t_fileinfo b) {
     if(a.st.st_ctime == b.st.st_ctime) {
-        return mx_cmp_name(a, b);
+        return !mx_cmp_name(a, b);
     }
     return a.st.st_ctime < b.st.st_ctime;
 }
 
 bool mx_cmp_mtime(t_fileinfo a, t_fileinfo b) {
     if(a.st.st_mtime == b.st.st_mtime) {
-        return mx_cmp_name(a, b);
+        return !mx_cmp_name(a, b);
     }
     return a.st.st_mtime < b.st.st_mtime;
 }
--- test.sh
+++ test.sh
@@ -4,8 +4,8 @@ RED='\033[0;31m'
 GRN='\033[0;32m'
 NOC='\033[0m'
 
-TEST=$(ls -ltc  >&1)
-RESULT=$(./uls -ltc  >&1)
+TEST=$(ls -R  >&1)
+RESULT=$(./uls -R  >&1)
 
 if [[ $RESULT == $TEST ]]
 then
